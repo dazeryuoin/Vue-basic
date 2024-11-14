c8bd62256d5bc402b7b9a1ee892ed61a93425ca7
Derril Agusti <tengahjawa31@gmail.com>
2024-11-14 15:28:51 +0700

    Event & method in Vue JS

--- chapter 1/app.js
+++ chapter 1/app.js
@@ -3,12 +3,21 @@ var app = new Vue({
   data: {
     maximum: 50,
     products: null,
+    cart: [],
   },
   mounted: function () {
     fetch("https://hplussport.com/api/products/order/price")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         this.products = data;
       });
+  },
+  /**
+   *
+   */
+  methods: {
+    addItem: function (product) {
+      this.cart.push(product);
+    },
   },
 });
